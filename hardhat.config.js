80fb3ede55ef439b82adc1a41ba5c13167360a89
Parva Parikh <58827015+Parvaparikh30@users.noreply.github.com>
2024-04-23 01:14:43 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 console.log("process", process.env)
 const RPC_URL = process.env.RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY_OWNER
-console.log("RPC_URL", RPC_URL)
-console.log("PRIVATE_KEY", PRIVATE_KEY)
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "amoy",
@@ -25,6 +24,7 @@ module.exports = {
       accounts: [PRIVATE_KEY]
     },
     sepolia: {
+      chainID: 11155111,
       url: RPC_URL,
       accounts: [PRIVATE_KEY]
     }
